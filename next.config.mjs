26d2f2c3aeea35af83d548fdd90c07834d581e71
ranaqasid <rqa166@gmail.com>
2024-12-22 12:50:12 +0500

    5

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+      // Ignore .map files during the build
+      config.module.rules.push({
+        test: /\.map$/,
+        use: 'ignore-loader',
+      });
+  
+      if (!isServer) {
+        // Prevent `chrome-aws-lambda` from being included in client-side bundles
+        config.resolve.alias['chrome-aws-lambda'] = false;
+      }
+  
+      return config;
+    },
+  };
+  
+  export default nextConfig;
+  
